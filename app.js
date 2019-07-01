dadcec58e0f41c56494997a0cd6abb30921135b5
doDomoGu <kaka_71936410@qq.com>
2019-07-01 15:51:14 +0800

    options 跨域

--- app.js
+++ app.js
@@ -10,6 +10,18 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,x-token");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  next();
+});
+
+app.options('*', function(req, res, next) {
+  res.send('')
+});
+
 app.use('/v1', v1Router);
 
 module.exports = app;
